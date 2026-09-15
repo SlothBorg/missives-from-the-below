884df5ec444a5b75202924b0ff0dfcaa44fcce7a
Slothborg <SlothBorg@users.noreply.github.com>
2026-09-14 21:36:45 -0400

    update new-post script

--- scripts/new-post.sh
+++ scripts/new-post.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-POSTS_DIR="${POSTS_DIR:-src/content/blog/draft}"
+POSTS_DIR="${POSTS_DIR:-src/content/blog/drafts}"
 
 read -rp "File name (no extension): " filename
 read -rp "Title: " title
